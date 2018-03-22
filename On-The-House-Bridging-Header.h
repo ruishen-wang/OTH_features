b6635792926744be3961abd37bfc64cbab8d2661
rmit-s3428088-Beier-NIE <s3428088@student.rmit.edu.au>
2018-03-22 17:20:37 +1100

    1ST COMMIT

---
+++ On-The-House-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  On-The-House-Bridging-Header.h
+//  On-The-House
+//
+//  Created by beier nie on 21/3/18.
+//  Copyright © 2018 RMIT. All rights reserved.
+//
+
+#ifndef On_The_House_Bridging_Header_h
+#define On_The_House_Bridging_Header_h
+#import <HWDownSelectedView.h>
+
+#endif /* On_The_House_Bridging_Header_h */
---
+++ On-The-House/OfferModel.swift
@@ -0,0 +1,27 @@
+//
+//  OfferModel.swift
+//  On-The-House
+//
+//  Created by beier nie on 22/3/18.
+//  Copyright © 2018 RMIT. All rights reserved.
+//
+
+import Foundation
+import CoreData
+import UIKit
+
+class OfferModel{
+
+    var name: String
+    var photo: UIImage?
+    var Description: String
+    
+    init(name:String, photo:UIImage, Description:String){
+        self.name = name
+        self.photo = photo
+        self.Description = Description
+    
+    }
+    
+    
+}
